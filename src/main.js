3bfb5210f55069bdac35cf1bcfa434f07a4214d2
zourong <1298452397@qq.com>
2018-08-16 11:40:20 +0800

    修改了评标办法页面

--- src/main.js
+++ src/main.js
@@ -10,14 +10,15 @@ Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
 // 全局路由守卫
+
 // router.beforeEach((to, from, next) => {
-	// 获取得token 
+	// 获取得token
 // 	let token = localStorage.getItem('mytoken')
 // 	// 如果已经登录，那我不干涉你，让你随便访问
 // 	if (token) {
-	// 判断得是否拥有得token 
+	// 判断得是否拥有得token
 // 		next()
-   // 
+   //
 // 	} else {
 // 		if (to.path !== '/login') {
 // 			// 如果没有登录，但你访问其他需要登录的页面，那我就让你跳到登录页面去
