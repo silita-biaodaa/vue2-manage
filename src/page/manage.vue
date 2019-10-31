b2de5750c5d191d1a90473735fa30ba0a153da48
fTaylorswift <1600538919@qq.com>
2019-10-31 17:04:31 +0800

    首页忘记密码展示;

--- src/page/manage.vue
+++ src/page/manage.vue
@@ -16,6 +16,12 @@
           <div class="logo">
             <img src="../assets/img/download_logo.png" />
           </div>
+          <el-menu-item index="home">
+            <template slot="title">
+              <i class="el-icon-s-home"></i>
+              <span>首页</span>
+            </template>
+          </el-menu-item>
           <el-submenu v-for="(submenu,i) in meunList"
            :key="i"
            :index="''+submenu.id"
@@ -33,6 +39,12 @@
               {{item.title}}
               </el-menu-item>
           </el-submenu>
+          <el-menu-item index="passWord">
+              <template slot="title">
+                <i class="el-icon-lock"></i>
+                <span>修改密码</span>
+              </template>
+            </el-menu-item>
           <!-- <el-submenu index="2">
             <template slot="title">
               <i class="el-icon-loading"></i>
